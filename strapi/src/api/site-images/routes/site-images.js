e6cec45c05ce2862f00c0bc686cd16d7b4ac3fdf
Bert Pintjens <bert.pintjens@hotmail.com>
2024-02-21 15:31:03 +0100

    try to set up development branch

---
+++ strapi/src/api/site-images/routes/site-images.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * site-images router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::site-images.site-images');
---
+++ strapi/src/api/site-images/services/site-images.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * site-images service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::site-images.site-images');
